4a38d00fc93516b23deecab2c7ead531d5aaf539
mm21 <mm21.dev@gmail.com>
2024-10-27 04:27:38 +0000

    Improve variant test

--- test/lib/test_alphabet.py
+++ test/lib/test_alphabet.py
@@ -1,58 +1,11 @@
 from pathlib import Path
-from typing import Generator
-import itertools
 
-from glyphsynth.lib.variants import BaseVariantExportFactory
 from glyphsynth.lib.alphabet.minimal import (
     LETTER_CLS_LIST,
-    UNIT,
-    BaseLetterGlyph,
-    LetterComboParams,
-    BaseLetterComboGlyph,
-    A,
-    M,
-    T,
 )
 
-from ..conftest import write_glyph, write_glyphs
-
-LETTERS = [
-    A,
-    M,
-    T,
-]
-
-
-class AMTComboParams(LetterComboParams):
-    letters: list[type[BaseLetterGlyph]]
-
-    @property
-    def desc(self) -> str:
-        return f"{'_'.join([l.__name__ for l in self.letters])}"
-
-
-class AMTComboGlyph[ParamsT: AMTComboParams](BaseLetterComboGlyph[ParamsT]):
-    def draw(self):
-        for letter_cls in self.params.letters:
-            self.draw_letter(letter_cls)
-
-
-class VariantFactoryTest(BaseVariantExportFactory[AMTComboGlyph]):
-    MATRIX_WIDTH = len(LETTERS)
-    SPACING = UNIT / 10
-    glyph_cls = AMTComboGlyph
-
-    def get_params_variants(self) -> Generator[AMTComboParams, None, None]:
-        for letter1, letter2, letter3 in itertools.product(
-            LETTERS, LETTERS, LETTERS
-        ):
-            yield AMTComboParams(letters=[letter1, letter2, letter3])
+from ..conftest import write_glyphs
 
 
 def test_letters(output_dir: Path):
     write_glyphs(output_dir, [letter_cls() for letter_cls in LETTER_CLS_LIST])
-
-
-def test_variants(output_dir: Path):
-    for spec in VariantFactoryTest():
-        write_glyph(output_dir / spec.path, spec.glyph)
---
+++ test/lib/test_variant.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+from typing import Generator
+import itertools
+
+from glyphsynth.lib.variants import BaseVariantExportFactory
+from glyphsynth.lib.alphabet.minimal import (
+    UNIT,
+    LetterParams,
+    BaseLetterGlyph,
+    LetterComboParams,
+    BaseLetterComboGlyph,
+    A,
+    M,
+    T,
+)
+
+from ..conftest import write_glyph, write_glyphs
+
+LETTERS = [
+    A,
+    M,
+    T,
+]
+
+COLORS = [
+    "black",
+    "red",
+    "green",
+    "blue",
+]
+
+
+class AMTComboParams(LetterComboParams):
+    letter1: type[BaseLetterGlyph]
+    letter2: type[BaseLetterGlyph]
+
+
+class AMTComboGlyph[ParamsT: AMTComboParams](BaseLetterComboGlyph[ParamsT]):
+    def draw(self):
+        # draw letters given by params
+        self.draw_letter(self.params.letter1)
+        self.draw_letter(self.params.letter2)
+
+
+class VariantFactory(BaseVariantExportFactory[AMTComboGlyph]):
+    MATRIX_WIDTH = len(COLORS)
+    SPACING = UNIT / 10
+    glyph_cls = AMTComboGlyph
+
+    # generate variants of letter combinations
+    def get_params_variants(self) -> Generator[AMTComboParams, None, None]:
+        for color, letter1, letter2 in itertools.product(
+            COLORS, LETTERS, LETTERS
+        ):
+            yield AMTComboParams(
+                letter_params=LetterParams(color=color),
+                letter1=letter1,
+                letter2=letter2,
+            )
+
+
+def test_variants(output_dir: Path):
+    for spec in VariantFactory():
+        write_glyph(output_dir / spec.path, spec.glyph)
