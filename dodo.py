a6ad447388287d6365b0e67ae408885adfc9cfc6
mm21 <mm21.dev@gmail.com>
2024-11-23 22:58:02 +0000

    Add format task

--- dodo.py
+++ dodo.py
@@ -84,3 +84,46 @@ def task_badges():
             COV_XML_PATH,
         ],
     )
+
+
+def task_format() -> Task:
+    """
+    Run formatters.
+    """
+
+    autoflake_args = [
+        "autoflake",
+        "--remove-all-unused-imports",
+        "--remove-unused-variables",
+        "-i",
+        "-r",
+        ".",
+    ]
+
+    isort_args = [
+        "isort",
+        ".",
+    ]
+
+    black_args = [
+        "black",
+        ".",
+    ]
+
+    toml_sort_args = [
+        "toml-sort",
+        "-i",
+        "pyproject.toml",
+    ]
+
+    return Task(
+        "format",
+        actions=[
+            " ".join(autoflake_args),
+            " ".join(isort_args),
+            " ".join(black_args),
+            " ".join(toml_sort_args),
+        ],
+        targets=[],
+        file_dep=[],
+    )
